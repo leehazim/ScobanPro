1d3e959cf0a9c6305e2822e842c95887a30e943d
seunghuyn <101630668+leehazim@users.noreply.github.com>
2022-05-14 09:22:21 +0900

    자작연습(2022.05.14)
    1. 코드 가독성을 위한 함수 분리
    2. 함수, 주석 연습
    3. 매직넘버 사용 최소화
    4. 박스, 목적지 카운트방식 변경

--- SelfCreateProject/MapMaker/main.cpp
+++ SelfCreateProject/MapMaker/main.cpp
@@ -1,32 +1,33 @@
-#include <Windows.h>
 #include <vector>
+#include "01.Function.h"
 #pragma comment(lib, "Msimg32")
 
-#define BLOCK_WIDTH 32
-#define BLOCK_HEIGHT 32
-#define MAX_WIDTH 10
-#define MAX_HEIGHT 15
-
-HBITMAP Tiles[5];
-HWND TileWnd[150];
-enum tag_Tile { WALL, BOX, MAN, GOAL, WAY };
-int countBox = 0, countGoal = 0, countStage = 0;
 std::vector<int*> Maps;
 
-struct tag_map {
-	int map[MAX_HEIGHT][MAX_WIDTH];
-	int stage;
-};
-
-LRESULT CALLBACK WndProc(HWND hwnd, UINT iMessage, WPARAM wParam, LPARAM lParam);
-LRESULT CALLBACK TileProc(HWND hwnd, UINT iMessage, WPARAM wParam, LPARAM lParam);
+// 윈도우클래스를 설정하기 위한 식별자
 LPCTSTR lpszClass = TEXT("MapMaker");
 LPCTSTR lpszTile = TEXT("Tile");
+
+// 윈도우인스턴스와 윈도우핸들 전역변수로 관리
 HINSTANCE g_hInst;
 HWND g_hMainWnd;
 HWND hStatic;
+HWND TileWnd[150];
 
+// Child윈도우들의 정보들 담아두기위한 변수들
+HBITMAP Tiles[5];
+int countBox = 0;
+int countGoal = 0;
+int countStage = 0;
 
+/// <summary>
+/// 작성일 : 2022년 5월 11일 
+/// 최종 수정일 : 2022년 5월 14일
+/// 
+/// 내용:
+/// 윈도우 진입함수 메인윈도우클래스 등록과 생성, 블록들의 정보들과 출력을 위한 윈도우클래스 등록
+/// 메시지 루프.
+/// </summary>
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow) {
 	HWND hWnd;
 	WNDCLASS ws;
@@ -61,46 +62,17 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmd
 	return (int)Msg.wParam;
 }
 
-HBITMAP LoadMyBitmap(HDC hdc, TCHAR* Path) {
-
-	HANDLE hFile;
-	DWORD FileSize, dwRead;
-	BITMAPFILEHEADER* fh;
-	HBITMAP hBit;
-	PVOID ih;
-
-	hFile = CreateFile(Path, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-	if (hFile == INVALID_HANDLE_VALUE) return NULL;
-	FileSize = GetFileSize(hFile, NULL);
-	fh = (BITMAPFILEHEADER*)malloc(FileSize);
-	ReadFile(hFile, fh, FileSize, &dwRead, NULL);
-	CloseHandle(hFile);
-
-	ih = ((PBYTE)fh + sizeof(BITMAPFILEHEADER));
-	hBit = CreateDIBitmap(hdc, (BITMAPINFOHEADER*)ih, CBM_INIT, (PBYTE)fh + fh->bfOffBits, (BITMAPINFO*)ih, DIB_RGB_COLORS);
-	free(fh);
-	return hBit;
-}
-
-void DrawBitmap(HDC hdc, int x, int y, HBITMAP hBit) {
-
-	HDC MemDC;
-	HBITMAP OldBitmap;
-	BITMAP bit;
-	int bx, by;
-	
-	MemDC = CreateCompatibleDC(hdc);
-	OldBitmap = (HBITMAP)SelectObject(MemDC, hBit);
-	GetObject(hBit, sizeof(BITMAP), &bit);
-	bx = bit.bmWidth;
-	by = bit.bmHeight;
-	
-	BitBlt(hdc, x, y, bx, by, MemDC, 0, 0, SRCCOPY);
-
-	SelectObject(MemDC, OldBitmap);
-	DeleteDC(MemDC);
-}
-
+/// <summary>
+/// 작성일 : 2022년 5월 11일
+/// 최종 수정일 : 2022년 5월 14일
+/// 
+/// 내용:
+/// 메인 윈도우 프로시저 비트맵을 불러오고 타일윈도우들을 생성하고,
+/// 박스와 목적지 개수를 표기해줄 스태틱윈도우 생성
+/// 
+/// 주의사항:
+///  최초 실행시 비트맵파일을 벽, 박스, 플레이어, 목적지 순으로 불러오기
+/// </summary>
 LRESULT CALLBACK WndProc(HWND hwnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
 
 	OPENFILENAME OFN;
@@ -119,7 +91,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 		OFN.lpstrFile = lpszStr;
 		OFN.lpstrFilter = TEXT("Bitmap File(*bmp)\0*.bmp\0");
 		OFN.nMaxFile = MAX_PATH;
-		for (int i = 0; i < 4; i++) {
+		for (int i = 0; i < COUNT_BITMAP; i++) {
 			if (GetOpenFileName(&OFN) != 0) {
 				hdc = GetDC(hwnd);
 				Tiles[i] = LoadMyBitmap(hdc, lpszStr);
@@ -127,9 +99,11 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 			}
 		}
 		for (int i = 0; i < MAX_HEIGHT; i++) {
-			for(int j = 0, count = 0; j < MAX_WIDTH; j++, count++)
+			for (int j = 0; j < MAX_WIDTH; j++) {
 				TileWnd[count] = CreateWindow(lpszTile, NULL, WS_CHILD | WS_VISIBLE | WS_BORDER,
-										  j*32, i*32, 32, 32, hwnd, (HMENU)NULL, g_hInst, NULL);
+											  j * 32, i * 32, 32, 32, hwnd, (HMENU)NULL, g_hInst, NULL);
+				count++;
+			}
 		}
 
 		wsprintf(str, TEXT("박스 개수 = %d, 골개수 = %d"), countBox, countGoal);
@@ -149,12 +123,21 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 	return DefWindowProc(hwnd, iMessage, wParam, lParam);
 }
 
+/// <summary>
+/// 작성일 : 2022년 5월 11일
+/// 최종 수정일 : 2022년 5월 14일
+/// 
+/// 내용:
+/// 타일윈도우 프로시저, 좌클릭시 타일이 변경,
+/// 현재 상태에 따라 바로 스태틱에 출력할 개수 변경
+/// 
+/// </summary>
 LRESULT CALLBACK TileProc(HWND hwnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
 
-	HDC hdc; PAINTSTRUCT ps;
 	int tmp;
+	HDC hdc; PAINTSTRUCT ps;
 	TCHAR str[128];
-	wsprintf(str, TEXT("박스 개수 = %d, 골개수 = %d"), countBox, countGoal);
+
 	switch (iMessage) {
 	case WM_CREATE:
 		SetWindowLongPtr(hwnd, 0, WALL);
@@ -162,14 +145,19 @@ LRESULT CALLBACK TileProc(HWND hwnd, UINT iMessage, WPARAM wParam, LPARAM lParam
 
 	case WM_LBUTTONDOWN:
 		tmp = GetWindowLongPtr(hwnd, 0);
-		if (tmp == BOX) countBox--;
-		else if (tmp == GOAL) countGoal--;
 		tmp++;
-		if (tmp == BOX) countBox++;
-		else if (tmp == GOAL) countGoal++;
-		else if (tmp >= WAY) tmp = WALL;
+		if (tmp > WAY) tmp = WALL;
 		SetWindowLongPtr(hwnd, 0, tmp);
 		InvalidateRect(hwnd, NULL, TRUE);
+		
+		countGoal = 0;
+		countBox = 0;
+		for(int i = 0; i< MAX_TILE; i++){
+			tmp = GetWindowLongPtr(TileWnd[i], 0);
+			if (tmp == GOAL) countGoal++;
+			else if (tmp == BOX) countBox++;
+		}
+
 		wsprintf(str, TEXT("박스 개수 = %d, 골개수 = %d"), countBox, countGoal);
 		SetWindowText(hStatic, str);
 		return 0;
